5f052a465244c4d6d9b2fbc9567daac0f2a9bd98
rayhan <156523736+TheRayhan009@users.noreply.github.com>
2024-02-10 10:14:13 +0600

    Update gussing_game.py

--- gussing_game.py
+++ gussing_game.py
@@ -1,10 +1,16 @@
 import random
 import os
+class color:
+    BOLD = '\033[1m'
+    END = '\033[0m'
+    
 point=0
-koko=0
+first_txt=0
 def start():
-    start1='''
     # this is the welcome text.You will see this text first when you start playing this game.
+    
+    start1='''
+    
 ____________________________________________________________________________________________________________
 |                                              WELCOME!!                                                    |
 |                                   This is a numbre gussing game.                                          |
@@ -17,37 +23,42 @@ ________________________________________________________________________________
 |                                                ENJOY!!                                                    |
 |___________________________________________________________________________________________________________|
 '''
-    print(start1)
+    big_and_bold_text = color.BOLD + start1.upper() + color.END
+    print(big_and_bold_text)
 
 def play_or_not():
-    play='''
     # This is a text asking you want to play again or not .
+    play='''
+
 ____________________________________________________________________________________________________________
 |                                          You want to play again?                                          |
 |                                               To yes press (y)                                            |           
 |                                               To no press (n)                                             |
 |___________________________________________________________________________________________________________|
 '''
-    print(play)
+    big_and_bold_text = color.BOLD + play.upper() + color.END
+    print(big_and_bold_text)
 def gogo():
-    play='''
     # This text will appear whenever you answer yes(y) to you want to play again.
+    play='''
+    
 ____________________________________________________________________________________________________________
 |                                          let's Goooo!!                                                    |           
 |___________________________________________________________________________________________________________|
 '''
-    print(play)
+    big_and_bold_text = color.BOLD + play.upper() + color.END
+    print(big_and_bold_text)
     
 while True:
     the_gussing_numbre=random.randint(1 , 1000)
-    x=0
+    x=29
     q=0
-    if koko==0:
+    if first_txt==0:
         start()
     else:
         gogo()
-    while x<30:
-        koko=1
+    while x>0:
+        first_txt=1
         num=int(input())
         if num==the_gussing_numbre:
             q=1
@@ -59,16 +70,17 @@ while True:
                 print("it's too long.")
             elif num<the_gussing_numbre:
                 print("it's too small.")
-        x+=1
+            print("Charles Remaining",x)
+        x-=1
     if q==0:
         point=0
         print("\n \n You defeated..!!  your point is : ",point)
         play_or_not()
-        yorno=input()
-        if yorno=="n":
+        yes_or_no=input()
+        if yes_or_no=="n" or yes_or_no=="N":
             print("The game is sucsecfully ended.!")
             break
-        if yorno=="y":
+        if yes_or_no=="y"or yes_or_no=="Y":
             continue
         else:
             print("error : Invalid input")
@@ -76,11 +88,11 @@ while True:
     elif q==1:
         print("you got it!! your point is : ",point)
         play_or_not()
-        yorno=input()
-        if yorno=="n":
+        yes_or_no=input()
+        if yes_or_no=="n" or yes_or_no=="N":
             print("The game is sucsecfully ended.!")
             break
-        if yorno=="y":
+        if yes_or_no=="y"or yes_or_no=="Y":
             continue
         else:
             print("error : Invalid input")
